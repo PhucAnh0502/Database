e0f8b71eb2643f4fc6fe7be5bf6697853db90098
Le Dong Canh Phu <canhphu131204@gmail.com>
2024-06-10 01:05:13 +0700

    Search product by name or category function

---
+++ source/Search,Add,Order,Cancel/search_product.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION search_products_by_name(product_name TEXT)
+RETURNS TABLE(prod_id INT, prod_name TEXT, category_name TEXT) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT p.prod_id, p.prod_name::text, c.category_name::text
+    FROM products p
+    JOIN categories c ON p.category_id = c.category_id
+    WHERE p.prod_name ILIKE '%' || product_name || '%';
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION search_products_by_category(product_category_name TEXT)
+returns table(prod_id INT, prod_name TEXT, category_name TEXT) AS $$
+BEGIN
+	return query
+	select p.prod_id, p.prod_name::text, c.category_name::text
+	from products p
+	join categories c on p.category_id = c.category_id
+	where c.category_name = product_category_name;
+end;
+$$ LANGUAGE plpgsql;
+
