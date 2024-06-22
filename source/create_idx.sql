ab8b467d247eafd7978b75fea288a3aaa628bf13
PhucAnh0502 <phucanh05022004@gmail.com>
2024-06-22 22:54:51 +0700

    Update create_idx.sql

--- source/create_idx.sql
+++ source/create_idx.sql
@@ -7,7 +7,7 @@ CREATE INDEX idx_order_detail_prod_id ON order_detail (prod_id);
 
 -- Tạo index cho các cột trong bảng products
 CREATE INDEX idx_products_prod_id ON products (prod_id);
-CREATE INDEX idx_products_dis_id ON order_detail (dis_id);
+CREATE INDEX idx_products_dis_id ON products (dis_id);
 
 -- Tạo index cho các cột trong bảng discounts
 CREATE INDEX idx_discounts_dis_id ON discounts (dis_id);
