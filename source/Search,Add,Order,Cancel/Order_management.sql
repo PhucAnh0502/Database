a1a06ca94ab5d3ecc1673079fec6493ac4c7c511
Le Dong Canh Phu <canhphu131204@gmail.com>
2024-06-23 01:53:47 +0700

    Update Order_management.sql

--- source/Search,Add,Order,Cancel/Order_management.sql
+++ source/Search,Add,Order,Cancel/Order_management.sql
@@ -5,7 +5,6 @@ CREATE TABLE transaction_history (
     product_id INT,
     quantity INT,
     price NUMERIC(10, 2),
-    discount_percent NUMERIC(5, 2),
     transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     action VARCHAR(50)
 );
@@ -15,32 +14,29 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF TG_OP = 'INSERT' THEN
         INSERT INTO transaction_history (
-            order_id, customer_id, product_id, quantity, price, discount_percent, action
+            order_id, customer_id, product_id, quantity, price, action
         ) VALUES (
             NEW.order_id, (SELECT cust_id FROM orders WHERE order_id = NEW.order_id),
             NEW.prod_id, NEW.quantity,
             (SELECT price FROM products WHERE prod_id = NEW.prod_id),
-            (SELECT dis_percent FROM discounts WHERE dis_id = NEW.dis_id),
             'INSERT'
         );
     ELSIF TG_OP = 'UPDATE' THEN
         INSERT INTO transaction_history (
-            order_id, customer_id, product_id, quantity, price, discount_percent, action
+            order_id, customer_id, product_id, quantity, price, action
         ) VALUES (
             NEW.order_id, (SELECT cust_id FROM orders WHERE order_id = NEW.order_id),
             NEW.prod_id, NEW.quantity,
             (SELECT price FROM products WHERE prod_id = NEW.prod_id),
-            (SELECT dis_percent FROM discounts WHERE dis_id = NEW.dis_id),
             'UPDATE'
         );
     ELSIF TG_OP = 'DELETE' THEN
         INSERT INTO transaction_history (
-            order_id, customer_id, product_id, quantity, price, discount_percent, action
+            order_id, customer_id, product_id, quantity, price, action
         ) VALUES (
             OLD.order_id, (SELECT cust_id FROM orders WHERE order_id = OLD.order_id),
             OLD.prod_id, OLD.quantity,
             (SELECT price FROM products WHERE prod_id = OLD.prod_id),
-            (SELECT dis_percent FROM discounts WHERE dis_id = OLD.dis_id),
             'DELETE');
     END IF;
 	RETURN NEW;
