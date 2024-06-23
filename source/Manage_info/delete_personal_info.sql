d56cba3faea5295ac4c798a90c0fa0d989af7882
BMTuna <146326813+TunaFishSlayer@users.noreply.github.com>
2024-06-23 10:08:26 +0700

    Update delete_personal_info.sql

--- source/Manage_info/delete_personal_info.sql
+++ source/Manage_info/delete_personal_info.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION delete_personal_info(p_id INTEGER)
+CREATE OR REPLACE FUNCTION delete_account(p_id INT)
 RETURNS TEXT AS $$
 DECLARE
     _customer_ID INT;
@@ -14,31 +14,40 @@ BEGIN
         RETURN 'Error: Personal ID does not exist';
     END IF;
  
+   
     SELECT seller_id INTO _seller_ID FROM sellers WHERE personal_id = p_id;
     SELECT cust_id INTO _customer_ID FROM customers WHERE personal_id = p_id;
+
     SELECT COALESCE(array_agg(prod_ID), '{}') INTO _product_IDs FROM products WHERE seller_ID = _seller_ID;
+    
     SELECT cart_id INTO cart_IDs FROM carts WHERE cust_id = _customer_ID;
+   
     SELECT COALESCE(array_agg(order_id), '{}') INTO o_id FROM orders WHERE cust_id = _customer_ID;
 
+  
     BEGIN
+		DELETE FROM comments WHERE cust_ID = _customer_ID;
+        DELETE FROM comments WHERE prod_ID = ANY(_product_IDs);
+        
         DELETE FROM order_detail WHERE prod_ID = ANY(_product_IDs);
         DELETE FROM order_detail WHERE order_id = ANY(o_id);
+        
         DELETE FROM cart_product WHERE cart_id = cart_IDs;
         DELETE FROM cart_product WHERE prod_ID = ANY(_product_IDs);
+        
         DELETE FROM products WHERE seller_ID = _seller_ID;
+        
         DELETE FROM carts WHERE cust_ID = _customer_ID;
-        DELETE FROM comments WHERE cust_ID = _customer_ID;
-        DELETE FROM comments WHERE prod_ID = ANY(_product_IDs);
+       
         DELETE FROM orders WHERE order_id = ANY(o_id);
+       
         DELETE FROM customers WHERE cust_ID = _customer_ID;
         DELETE FROM sellers WHERE seller_ID = _seller_ID;
+        
         DELETE FROM personal_info WHERE personal_id = p_id;
         
         RETURN 'Account deleted successfully';
-    EXCEPTION WHEN OTHERS THEN
-
-        ROLLBACK;
-        RETURN 'Error: Could not delete account due to an unexpected error';
+    
     END;
 END;
 $$ LANGUAGE plpgsql;
