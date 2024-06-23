d0a5fea72eb8b67e24032452a0020b1e2fc04959
BMTuna <146326813+TunaFishSlayer@users.noreply.github.com>
2024-06-23 09:36:38 +0700

    Update delete_personal_info.sql

--- source/Manage_info/delete_personal_info.sql
+++ source/Manage_info/delete_personal_info.sql
@@ -2,27 +2,43 @@ CREATE OR REPLACE FUNCTION delete_personal_info(p_id INTEGER)
 RETURNS TEXT AS $$
 DECLARE
     _customer_ID INT;
-	_seller_ID INT;
+    _seller_ID INT;
     _product_IDs INT[];
-	
+    cart_IDs INT;
+    o_id INT[];
+    info_exists BOOLEAN;
 BEGIN 
+    
+    SELECT EXISTS (SELECT 1 FROM personal_info WHERE personal_id = p_id) INTO info_exists;
+    IF NOT info_exists THEN
+        RETURN 'Error: Personal ID does not exist';
+    END IF;
+ 
     SELECT seller_id INTO _seller_ID FROM sellers WHERE personal_id = p_id;
     SELECT cust_id INTO _customer_ID FROM customers WHERE personal_id = p_id;
-    SELECT array_agg(prod_ID) INTO _product_IDs FROM products WHERE seller_ID = _seller_ID;
-    
-    
-    DELETE FROM order_detail WHERE prod_ID = ANY(_product_IDs);
-    DELETE FROM cart_product WHERE prod_ID = ANY(_product_IDs);
-    DELETE FROM products WHERE seller_ID = _seller_ID;
-    DELETE FROM sellers WHERE seller_ID = _seller_ID;
-	DELETE FROM customers WHERE cust_ID = _customer_ID;
-	DELETE FROM orders WHERE cust_ID = _customer_ID;
-	DELETE FROM comments WHERE cust_ID = _customer_ID;
-	DELETE FROM carts WHERE cust_ID = _customer_ID;
-	DELETE FROM personal_info WHERE personal_id = p_id;
-	
-	RETURN 'Account deleted successfully';
+    SELECT COALESCE(array_agg(prod_ID), '{}') INTO _product_IDs FROM products WHERE seller_ID = _seller_ID;
+    SELECT cart_id INTO cart_IDs FROM carts WHERE cust_id = _customer_ID;
+    SELECT COALESCE(array_agg(order_id), '{}') INTO o_id FROM orders WHERE cust_id = _customer_ID;
+
+    BEGIN
+        DELETE FROM order_detail WHERE prod_ID = ANY(_product_IDs);
+        DELETE FROM order_detail WHERE order_id = ANY(o_id);
+        DELETE FROM cart_product WHERE cart_id = cart_IDs;
+        DELETE FROM cart_product WHERE prod_ID = ANY(_product_IDs);
+        DELETE FROM products WHERE seller_ID = _seller_ID;
+        DELETE FROM carts WHERE cust_ID = _customer_ID;
+        DELETE FROM comments WHERE cust_ID = _customer_ID;
+        DELETE FROM comments WHERE prod_ID = ANY(_product_IDs);
+        DELETE FROM orders WHERE order_id = ANY(o_id);
+        DELETE FROM customers WHERE cust_ID = _customer_ID;
+        DELETE FROM sellers WHERE seller_ID = _seller_ID;
+        DELETE FROM personal_info WHERE personal_id = p_id;
+        
+        RETURN 'Account deleted successfully';
+    EXCEPTION WHEN OTHERS THEN
+
+        ROLLBACK;
+        RETURN 'Error: Could not delete account due to an unexpected error';
+    END;
 END;
 $$ LANGUAGE plpgsql;
-
-select delete_personal_info(10001)
