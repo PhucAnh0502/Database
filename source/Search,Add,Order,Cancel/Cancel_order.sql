45109e4aeb0438c905b6b555c47617addd51f5a9
Le Dong Canh Phu <canhphu131204@gmail.com>
2024-06-23 16:10:33 +0700

    Update Cancel_order.sql

--- source/Search,Add,Order,Cancel/Cancel_order.sql
+++ source/Search,Add,Order,Cancel/Cancel_order.sql
@@ -1,6 +1,15 @@
 CREATE OR REPLACE FUNCTION cancel_order(o_id INT)
-RETURNS VOID AS $$
+RETURNS TEXT AS $$
+DECLARE
+    o_id_exists BOOLEAN;
 BEGIN
+    -- Kiểm tra xem order_id có tồn tại
+    SELECT EXISTS (SELECT 1 FROM orders WHERE order_id = o_id) INTO o_id_exists;
+    
+    -- Nếu order_id không tồn tại, trả về thông báo lỗi
+    IF NOT o_id_exists THEN
+        RETURN 'Error: Order ID does not exist';
+    END IF;
     -- Xóa các chi tiết đơn hàng từ bảng order_detail
     DELETE FROM order_detail
     WHERE order_id = o_id;
@@ -8,10 +17,6 @@ BEGIN
     -- Xóa đơn hàng từ bảng orders
     DELETE FROM orders
     WHERE order_id = o_id;
-
-    -- Kiểm tra xem đơn hàng đã được hủy thành công chưa
-    IF NOT FOUND THEN
-        RAISE EXCEPTION 'Order ID % does not exist', o_id;
-    END IF;
+	return 'Cancel Successfully';
 END;
 $$ LANGUAGE plpgsql;
